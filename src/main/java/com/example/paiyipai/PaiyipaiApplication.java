2c2d993e6301c6904b0b20eab9e4ecd0ffc572ac
Yawei Zhang <yawei.zhang@rea-group.com>
2022-08-13 13:47:06 +0800

    Project initiated

---
+++ src/main/java/com/example/paiyipai/PaiyipaiApplication.java
@@ -0,0 +1,13 @@
+package com.example.paiyipai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaiyipaiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaiyipaiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/paiyipai/PaiyipaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.paiyipai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaiyipaiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
